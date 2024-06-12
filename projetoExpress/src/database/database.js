03fee5ba8ab2eb5d46cf910c2832165d654308a8
Pericles Barreto <132623133+pericles81@users.noreply.github.com>
2024-06-12 01:08:42 -0300

    Update database.js

--- projetoExpress/src/database/database.js
+++ projetoExpress/src/database/database.js
@@ -2,6 +2,6 @@ import  {mongoose, connect } from 'mongoose'
 
 export default async function connectDatabase() {
     await mongoose.connect(
-        process.env.MONGODB_URI
+        'mongodb+srv://merrd78:BfrrmSNPFftJh5lH@cluster0.tn55sdm.mongodb.net/Cluster0?retryWrites=true&w=majority'
     )    
 }
